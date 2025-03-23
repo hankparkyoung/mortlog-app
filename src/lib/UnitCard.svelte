b34182981986e125bbaabb5e7cc79dbc561b5167
Hank <hankparkyoung@gmail.com>
2025-03-23 02:19:27 -0400

    feature: show all units on create game page with selections enabled

--- src/lib/UnitCard.svelte
+++ src/lib/UnitCard.svelte
@@ -1,15 +1,98 @@
 <script>
-  let { unit } = $props();
-  let {unit_name: name, cost, traits } = unit;
+  let { unit, toggleUnitSelection, selectedUnitIds, isMini } = $props();
+  let {unit_id: id, unit_name: name, cost, traits } = unit;
+  let isSelected = $derived(selectedUnitIds.includes(id));
+  let cost_style = `unit-cost-${cost}`;
 </script>
 
-<div class="unit-card">
-  <h3>{name}</h3>
-  <!-- temporary cost styling -->
-  <p>Cost: {cost}</p>
-  <ul>
-    {#each traits as trait}
-      <li>{trait.trait_name}</li>
-    {/each}
-  </ul>
+<div
+  class="unit-card"
+  class:mini-card={isMini}
+>
+  <button onclick={() => toggleUnitSelection(id)}>
+    <h3
+      class="title {cost_style}"
+      class:selected-cost-1={isSelected && unit.cost === 1}
+      class:selected-cost-2={isSelected && unit.cost === 2}
+      class:selected-cost-3={isSelected && unit.cost === 3}
+      class:selected-cost-4={isSelected && unit.cost === 4}
+      class:selected-cost-5={isSelected && unit.cost === 5}
+    >{name}</h3>
+    {#if !isMini}
+      <ul>
+        {#each traits as trait}
+          <li>{trait.trait_name}</li>
+        {/each}
+      </ul>
+    {/if}
+  </button>
 </div>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  button {
+    all: unset;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+  }
+
+  .unit-card {
+    border: 2px solid black;
+    display: block;
+    height: 106px;
+    margin: 4px;
+    width: 140px;
+  }
+  .mini-card {
+    height: 42px;
+  }
+  ul {
+    display: block;
+    list-style: none;
+    margin: 4px;
+  }
+  li {
+    margin-right: 12px;
+  }
+  .unit-cost-1 {
+    background-color: #bbbbbb88;
+  }
+  .selected-cost-1 {
+    background-color: #a3a3a3;
+  }
+  .unit-cost-2 {
+    background-color: #14CC7388;
+  }
+  .selected-cost-2 {
+    background-color: #14CC73;
+  }
+  .unit-cost-3 {
+    background-color: #54C3FF88;
+  }
+  .selected-cost-3 {
+    background-color: #54C3FF;
+  }
+  .unit-cost-4 {
+    background-color: #DE0EBD88;
+  }
+  .selected-cost-4 {
+    background-color: #ff42e0;
+  }
+  .unit-cost-5 {
+    background-color: #FFC43088;
+  }
+  .selected-cost-5 {
+    background-color: #FFC430;
+  }
+
+  .title {
+    padding: 8px;
+  }
+</style>
--- src/routes/create-game/+page.svelte
+++ src/routes/create-game/+page.svelte
@@ -3,12 +3,126 @@
 
   let { data } = $props();
   let { units } = data;
+  let unitsByCost = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+  };
+  units.forEach(unit => {
+    unitsByCost[unit.cost].push(unit);
+  });
+
+  let selectedUnitIds = $state([]);
+  const toggleUnitSelection = unitId => {
+    const index = selectedUnitIds.indexOf(unitId);
+    if (index === -1) {
+      selectedUnitIds.push(unitId);
+    } else {
+      selectedUnitIds.splice(index, 1);
+    }
+  };
+
+  $inspect(selectedUnitIds);
+
 </script>
 
 <h1>Create Game</h1>
 
-<div class="units-container">
-  {#each units as unit}
-    <UnitCard unit={unit} />
-  {/each}
+<div class="create-game">
+  <div class="unit-selection">
+    <h3 class="subtitle">Select Units</h3>
+    <div class="units-container">
+      <div class="units-by-cost">
+        {#each unitsByCost[1] as unit}
+          <UnitCard
+            unit={unit}
+            toggleUnitSelection={toggleUnitSelection}
+            selectedUnitIds={selectedUnitIds}
+          />
+        {/each}
+      </div>
+      <div class="units-by-cost">
+        {#each unitsByCost[2] as unit}
+          <UnitCard
+            unit={unit}
+            toggleUnitSelection={toggleUnitSelection}
+            selectedUnitIds={selectedUnitIds}
+          />
+        {/each}
+      </div>
+      <div class="units-by-cost">
+        {#each unitsByCost[3] as unit}
+          <UnitCard
+            unit={unit}
+            toggleUnitSelection={toggleUnitSelection}
+            selectedUnitIds={selectedUnitIds}
+          />
+        {/each}
+      </div>
+      <div class="units-by-cost">
+        {#each unitsByCost[4] as unit}
+          <UnitCard
+            unit={unit}
+            toggleUnitSelection={toggleUnitSelection}
+            selectedUnitIds={selectedUnitIds}
+          />
+        {/each}
+      </div>
+      <div class="units-by-cost">
+        {#each unitsByCost[5] as unit}
+          <UnitCard
+            unit={unit}
+            toggleUnitSelection={toggleUnitSelection}
+            selectedUnitIds={selectedUnitIds}
+          />
+        {/each}
+      </div>
+    </div>
+  </div>
+  <div class="current-game-state">
+    <h3 class="subtitle">Current Units</h3>
+    {#if selectedUnitIds.length === 0}
+      <p class="description">Units you select will show up here.</p>
+    {/if}
+    <div class="units-by-cost">
+      {#each units as unit}
+        {#if selectedUnitIds.includes(unit.unit_id)}
+          <UnitCard
+            unit={unit}
+            toggleUnitSelection={toggleUnitSelection}
+            selectedUnitIds={selectedUnitIds}
+            isMini={true}
+          />
+        {/if}
+      {/each}
+    </div>
+    <h3 class="subtitle">Active Traits</h3>
+  </div>
 </div>
+
+<style lang="scss">
+  .create-game {
+    background-color: #eeeeee;
+    display: flex;
+    padding: 8px;
+  }
+  .unit-selection, .current-game-state {
+    width: 50%;
+  }
+
+  .subtitle, .description {
+    margin: 0 0 8px 4px;
+    padding: 0;
+  }
+  .units-container {
+    display: block;
+    margin: none;
+  }
+  .units-by-cost {
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 8px;
+  }
+</style>
